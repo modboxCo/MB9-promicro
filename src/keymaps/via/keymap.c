68a72e20a8e0cd84cfcb43ed29bf017b31b243b2
modboxCo <modboxlabs@gmail.com>
2026-07-30 14:53:51 -0500

    Update keymap.c
    layer color change added

--- src/keymaps/via/keymap.c
+++ src/keymaps/via/keymap.c
@@ -19,3 +19,24 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     )
 };
 
+layer_state_t layer_state_set_user(layer_state_t state) {
+    switch (get_highest_layer(state)) {
+    case 1:
+        // Capa 1: Azul
+        rgblight_sethsv_at(170, 255, 200, 0);
+        break;
+    case 2:
+        // Capa 2: Rojo
+        rgblight_sethsv_at(0, 255, 200, 0);
+        break;
+    case 3:
+        // Capa 3: Verde
+        rgblight_sethsv_at(85, 255, 200, 0);
+        break;
+    default: // Capa 0
+        // Capa 0: cyan
+        rgblight_sethsv_at(128, 255, 200, 0);
+        break;
+    }
+    return state;
+}
